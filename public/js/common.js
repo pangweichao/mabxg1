3b6b6b4d7546ee5cee2ae3f3af4d03a682918153
zuodao <123456@qq.com>
2017-08-30 16:51:38 +0800

    修改

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,44 @@
 
-	NProgress.start();
+define(['jquery','template','cookie'],function($,template){
+    // NProgress.start();
 
-	NProgress.done();
-
+	// NProgress.done();
+    //控制左边导航栏
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+     //退出功能
+	 $("#logoutBtn").click(function(){
+            $.ajax({
+                type:'post',
+                url:'/api/logout',
+              
+                dataType:'json',
+                success:function(data){
+                    if(data.code==200){
+                       
+                        location.href='/main/login';
+                    }
+                }
+            })
+            return false;
+        })
+
+	 var sessionId=$.cookie('PHPSESSID');
+	 console.log(sessionId);
+	 if(!sessionId&&location.pathname!='/main/login'){
+	 	location.href='/main/login';
+	 }
+
+	 //获取用户登录信息，并填充页面
+	 var cookie=$.cookie('loginInfo');
+	 var loginInfo=cookie?JSON.parse(cookie):{};
+	 // $('.profile img').attr('src',loginInfo.tc_avatar);
+	 // $('.profile h4').html(loginInfo.tc_name);
+	 // var tpl='<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+	  var tpl = '<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+     var html=template.render(tpl,loginInfo);
+     $('#profileId').html(html);
+});
+
+	
---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+define(['jquery','cookie'],function($){
+
+	//
+     $("#loginBtn").click(function(){
+
+            $.ajax({
+                type:'post',
+                url:'/api/login',
+                data:$("#loginForm").serialize(),
+                dataType:'json',
+                success:function(data){
+                	console.log(data)
+                    if(data.code==200){
+                        console.log('正确')
+                       //存储用户信息到cookie
+                       $.cookie('loginInfo',JSON.stringify(data.result),{path:'/'})
+                        location.href='/main/index';
+                    }else{
+                        alert("用户名或者密码错误");
+                    }
+                }
+            })
+            return false;
+        })
+
+})
